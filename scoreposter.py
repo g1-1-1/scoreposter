404ac995a936730e28605d1984ad8041ee753b2d
jupiter <syl@waifu.club>
2023-01-04 20:40:15 +1100

    fix: accuracy calculation

--- scoreposter.py
+++ scoreposter.py
@@ -26,10 +26,34 @@ mods = [
 ]
 
 def int_to_readable(value: int) -> List[str]:
+    mod_names = [name for mod, name in mods if value & mod]
+
+    # Check for invalid combinations
+    if "SD" in mod_names and "PF" in mod_names:
+        mod_names.remove("SD")
+        return mod_names
+    if "DT" in mod_names and "NC" in mod_names:
+        # Remove "DT" and return "HD" and "NC"
+        mod_names.remove("DT")
+        return mod_names
+
+    return mod_names
+
+def int_to_santised_int(value: int) -> int:
     if value == 0:
-        return []
+        return 0
     else:
-        return [name for mod, name in mods if value & mod]
+        mods_to_check = [1024, 512, 256, 64, 16, 8, 2] # FL, NC, HT, DT, HR, HD, EZ
+        combined_value = 0
+        for mod in mods_to_check:
+            if value & mod:
+                combined_value |= mod
+        return combined_value
+
+def calculate_accuracy(perfect_count, hundred_count, fifty_count, misses):
+    accuracy = (perfect_count * 300 + hundred_count * 100 + fifty_count * 50) / (perfect_count + fifty_count + hundred_count + misses) / 300 * 100
+    return accuracy
+
 
 # define command line arguments using the argparse module
 parser = argparse.ArgumentParser()
@@ -46,17 +70,18 @@ print("making initial score request...")
 initial_data = inital_response.json()
 
 # extract the relevant information from the response
-beatmap_id, score_max, n300, n100, n50, nmiss, perfect, int_mods = (
+beatmap_id, rank, score_max, n300, n100, n50, nmiss, perfect, int_mods = (
     initial_data[0]["beatmap_id"], 
-    initial_data[0]["maxcombo"], 
+    initial_data[0]["rank"], 
+    int(initial_data[0]["maxcombo"]), 
     int(initial_data[0]["count300"]), 
     int(initial_data[0]["count100"]), 
-    int(initial_data[0]["count100"]), 
+    int(initial_data[0]["count50"]), 
     int(initial_data[0]["countmiss"]), 
     initial_data[0]["perfect"], 
-    int(initial_data[0]["enabled_mods"])
+    int_to_santised_int(int(initial_data[0]["enabled_mods"]))
 )
-accuracy = format(min(100.0 * ((n300 * 300.0) + (n100 * 100.0) + (n50 * 50.0)) / ((n300 + n100 + n50 + nmiss) * 300.0), 100), '.2f')
+
 readable_mods = int_to_readable(int(int_mods))
 
 print("made!")
@@ -65,13 +90,21 @@ print("making the score's map request...")
 map_response = requests.get(f"https://osu.ppy.sh/api/get_beatmaps?k={api_key}&b={beatmap_id}&limit=1")
 map_data = map_response.json()
 
-artist, title, creator, diff, map_max = (
+artist, title, creator, diff, map_max, circles, sliders, spinners, mode = (
     map_data[0]["artist"],
     map_data[0]["title"],
     map_data[0]["creator"],
     map_data[0]["version"],
-    map_data[0]["max_combo"],
+    int(map_data[0]["max_combo"]),
+    int(map_data[0]["count_normal"]),
+    int(map_data[0]["count_slider"]),
+    int(map_data[0]["count_spinner"]),
+    int(map_data[0]["mode"])
 )
+
+total_objects = circles + sliders + spinners
+print(total_objects)
+accuracy = min(100.0 * ((n300 * 300.0) + (n100 * 100.0) + (n50 * 50.0)) / ((n300 + n100 + n50 + nmiss) * 300.0), 100)
 
 print("made!")
 print("creating the scorepost...")
@@ -85,30 +118,32 @@ with open("beatmap.osu", "wb") as f:
         f.write(chunk)
 
 map = Beatmap(bytes=open("beatmap.osu", "rb").read())
-calc = Calculator(mode=0, mods=int(int_mods))
+calc = Calculator(mode=mode)
 calc.set_acc(accuracy)
-calc.set_n300(n300)
-calc.set_n100(n100)
-calc.set_n50(n50)
+calc.set_mods(int_mods)
 calc.set_n_misses(nmiss)
-calc.set_combo(int(score_max))
+calc.set_combo(score_max)
 
 pp = calc.performance(map)
 sr = round(float(pp.difficulty.stars), 2)
 
-if perfect == 1:
-    combo = "FC"
+if score_max - 20 >= map_max is True:
+    combo = "FC "
     max_pp_string = ""
+    miss_string = ""
 else:
     miss_string = f" {nmiss}❌ "
-    combo = f"{score_max}x/{map_max}x"
+    combo = f"{int(score_max):,}x/{int(map_max):,}x"
     calc.set_n_misses(0)
     calc.set_combo(int(map_max))
+    calc.set_mods(int_mods)
     max_pp = calc.performance(map)
-    max_pp_string = f"({round(max_pp.pp)}pp if FC)"
+    max_pp_string = f"({round(max_pp.pp):,}pp if FC)"
 
-scorepost = f"{args.username} | {artist} - {title} [{diff}] (mapped by {creator}, {sr}*){mods} {formatted_accuracy}% {combo}{miss_string}{round(pp.pp)}pp {max_pp_string} "
+print()
+
+scorepost = f"{args.username} | {artist} - {title} [{diff}] (mapped by {creator}, {sr}⭐️){mods} {accuracy:.2f}% {combo}{miss_string}{round(pp.pp):,}pp {max_pp_string} ".replace("%20", " ").replace("HDDTNC", "HDNC")
 
 # print the scorepost to the console
-print(f"\n{scorepost}")
+print(f"{scorepost}")
 print("\ncompleted!")
